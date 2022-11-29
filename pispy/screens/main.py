a20523a966124e93a71477e247daf58c982ed986
Dave Pearson <davep@davep.org>
2022-11-29 16:38:52 +0000

    :sparkles: Add a datatable of packages to the main screen

--- pispy/screens/main.py
+++ pispy/screens/main.py
@@ -4,7 +4,11 @@
 # Textual imports.
 from textual.app     import ComposeResult
 from textual.screen  import Screen
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, DataTable
+
+##############################################################################
+# Local imports.
+from ..data.stats import Packages
 
 ##############################################################################
 class Main( Screen ):
@@ -17,6 +21,18 @@ class Main( Screen ):
             ComposeResult: The main screen's layout.
         """
         yield Header()
+        self.top_100 = DataTable[ str ]( id="top100", zebra_stripes=True )
+        yield self.top_100
         yield Footer()
+
+    async def get_top_100( self ) -> None:
+        """Populate the main data grid with the top 100 packages."""
+        self.top_100.add_columns( "Package", "Size" )
+        for package in await Packages.top_100():
+            self.top_100.add_row( package.name, f"{package.size:,}" )
+
+    def on_mount( self ) -> None:
+        """Start to populate the application on startup."""
+        self.call_later( self.get_top_100 )
 
 ### main.py ends here
