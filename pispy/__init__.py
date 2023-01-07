0045db816397a5608bf604668fc101fb65b83bb6
Dave Pearson <davep@davep.org>
2023-01-07 10:27:17 +0000

    :bookmark: v0.3.0

--- pispy/__init__.py
+++ pispy/__init__.py
@@ -3,11 +3,11 @@
 ######################################################################
 # Main app information.
 __author__     = "Dave Pearson"
-__copyright__  = "Copyright 2022, Dave Pearson"
+__copyright__  = "Copyright 2022-2023, Dave Pearson"
 __credits__    = [ "Dave Pearson" ]
 __maintainer__ = "Dave Pearson"
 __email__      = "davep@davep.org"
-__version__    = "0.2.0"
+__version__    = "0.3.0"
 __licence__    = "GPLv3+"
 
 ### __init__.py ends here
