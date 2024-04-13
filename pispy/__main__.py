ad69875d0b7b4af71501fd5a856c4a61e4fec4d6
Dave Pearson <davep@davep.org>
2024-04-13 14:57:28 +0100

    :sparkles: Switch command line to proper argparse use
    Add a version and help display, rework the general layout of the startup
    code, and also have the app work inline if a package name is passed on the
    command line.

--- pispy/__main__.py
+++ pispy/__main__.py
@@ -1,8 +1,57 @@
 """Main entry point for running the application."""
 
 ##############################################################################
+# Python imports.
+from argparse import ArgumentParser, Namespace
+
+##############################################################################
+# Textual imports.
+from textual import __version__ as __textual_version__
+
+##############################################################################
 # Local imports.
-from .app import run
+from . import __version__
+from .app import PISpy
+
+
+##############################################################################
+def get_args() -> Namespace:
+    """Get the command line arguments.
+
+    Returns:
+        The parsed command line arguments.
+    """
+    parser = ArgumentParser(
+        prog="pispy",
+        description="Look up package information on PyPI.",
+        epilog=f"v{__version__}",
+    )
+
+    # Add the package argument.
+    parser.add_argument(
+        "package",
+        nargs="?",
+        help="A package to look up",
+    )
+
+    # Add --version
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Show version information.",
+        action="version",
+        version=f"%(prog)s {__version__} (Textual v{__textual_version__})",
+    )
+
+    # Return the arguments.
+    return parser.parse_args()
+
+
+##############################################################################
+def run() -> None:
+    """Run the application."""
+    PISpy(get_args().package).run(inline=get_args().package is not None)
+
 
 ##############################################################################
 # Run the app if we're being called on as the main entry point.
